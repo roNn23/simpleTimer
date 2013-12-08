c6d2a73b9688d36cda9e7bdb5f25025c5ea41034
Ronny Neefe <ronny@neefe.org>
2013-12-08 15:59:44 +0100

    added save routine

--- simpleTimer.js
+++ simpleTimer.js
@@ -4,34 +4,18 @@ var SimpleTimer = {};
 SimpleTimer = (function() {
   'use strict';
 
-  var self, recentTimerID, timers, intervals, formElement, titleInput, submit, runningTimersMsg;
+  var self, timers, intervals, formElement, titleInput, submit, runningTimersMsg;
 
   self = SimpleTimer || {};
 
-  recentTimerID    = 0;
-  timers           = [];
   intervals        = [];
   formElement      = {};
   titleInput       = {};
   submit           = {};
   runningTimersMsg = {};
 
-  function isTimerRunning() {
-    var i, timerself;
-
-    for (i = timers.length - 1; i >= 0; i -= 1) {
-      timerself = timers[i];
-
-      if(timerself.endTime === 0) {
-        return true;
-      }
-    }
-
-    return false;
-  }
-
   function stopTimer(timerID) {
-    var endTime, finishedTimersHeadline, finishedTimersMsg, timer;
+    var endTime;
 
     clearInterval(intervals[timerID]);
 
@@ -39,23 +23,14 @@ SimpleTimer = (function() {
 
     timers[timerID].endTime = endTime;
 
-    finishedTimersHeadline = self.lib._('.finishedTimers h3', true);
-    finishedTimersMsg = self.lib._('.finishedTimers .emptyMessage', true);
-
-    timer = self.lib._('p#timer' + timerID, true);
-    self.lib.insertAfter(finishedTimersHeadline, timer);
-
-    self.lib.addClass('hidden', finishedTimersMsg);
-
-    if(!isTimerRunning()) {
-      runningTimersMsg.classList.remove('hidden');
-    }
+    self.dataHandler.set('timers', timers);
   }
 
   function checkForStorageSupport() {
     try {
       return 'localStorage' in window && window['localStorage'] !== null;
     } catch (e) {
+      alert('browser not supported.');
       return false;
     }
   }
@@ -68,7 +43,7 @@ SimpleTimer = (function() {
 
   function bindEvents() {
     self.lib.on('keyup', titleInput, function() {
-      if(titleInput.value !== '') {
+      if (titleInput.value !== '') {
         submit.disabled = false;
       } else {
         submit.disabled = true;
@@ -77,7 +52,7 @@ SimpleTimer = (function() {
 
     self.lib.on('click', submit, function(e) {
       e.preventDefault();
-      startNewTimer();
+      startTimer();
     });
   }
 
@@ -90,26 +65,24 @@ SimpleTimer = (function() {
     submit.disabled = true;
   }
 
-  function startNewTimer() {
+  function startTimer() {
+    var startTime, endTime, titleOfTimer, timerID, newTimer, timersLength;
 
-    var startTime, titleOfTimer, timerID, newTimer;
-
-    startTime = new Date().getTime();
+    startTime    = new Date().getTime();
+    endTime      = 0;
     titleOfTimer = getTitleOfTimer();
-    timerID = timers.length;
 
     newTimer = {
-      'id': timerID,
       'titleName': titleOfTimer,
       'startTime': startTime,
-      'endTime': 0
+      'endTime': endTime
     };
 
-    timers.push(newTimer);
+    timers = self.model.getTimers();
+    timersLength = timers.push(newTimer);
+    timerID = timersLength - 1;
 
-    createNewTimer(timerID);
-
-    startIntervalForTimer(timerID);
+    self.dataHandler.set('timers', timers);
 
     resetForm();
   }
@@ -122,25 +95,6 @@ SimpleTimer = (function() {
     return titleInput.value;
   }
 
-  function createNewTimer(timerID) {
-
-    var timerElement, runningTimersHeadline;
-
-    runningTimersHeadline = self.lib._('.runningTimers h3', true);
-    runningTimersMsg = self.lib._('.runningTimers .emptyMessage', true);
-    self.lib.addClass('hidden', runningTimersMsg);
-
-    timerElement = document.createElement('p');
-    timerElement.id = 'timer' + timerID;
-    timerElement.innerHTML =
-      'Timer "' + timers[timerID].titleName + '": ' +
-      '<span>00:00:00:000</span> ' +
-      '<a href="#" onclick="SimpleTimer.stop(' + timerID + ');">Stop</a>'
-    ;
-
-    self.lib.insertAfter(runningTimersHeadline, timerElement);
-  }
-
   function startIntervalForTimer(timerID) {
     intervals[timerID] = setInterval(function() {
       showTimer(timerID);
@@ -150,23 +104,36 @@ SimpleTimer = (function() {
   function showTimer(timerID) {
     var times, timerSpan;
 
-    times = getTimes(timerID);
-
     timerSpan = self.lib._('p#timer' + timerID + ' span', true);
-    timerSpan.innerHTML =
-      times.hours + ':' +
-      times.minutes + ':' +
-      times.seconds + ':' +
-      times.milliseconds
-    ;
+    timerSpan.innerHTML = getFormatedTime(timerID);
   }
 
-  function getTimes(timerID) {
-    var endTime, milliseconds, date, seconds, minutes, hours, duration;
+  function getFormatedTime(timerID) {
+    var duration, timeString;
 
-    endTime = new Date().getTime();
+    duration = getDurationOfTimer(timerID);
 
-    milliseconds = endTime - timers[timerID].startTime;
+    timeString =
+      duration.hours + ':' +
+      duration.minutes + ':' +
+      duration.seconds + ':' +
+      duration.milliseconds
+    ;
+
+    return timeString;
+  }
+
+  function getDurationOfTimer(timerID) {
+    var timer, endTime, milliseconds, date, seconds, minutes, hours, duration;
+
+    timer = timers[timerID];
+
+    if(timer.endTime === 0)
+      endTime = new Date().getTime();
+    else
+      endTime = timer.endTime;
+
+    milliseconds = endTime - timer.startTime;
 
     date = new Date(milliseconds);
     milliseconds = date.getMilliseconds();
@@ -184,13 +151,85 @@ SimpleTimer = (function() {
     return duration;
   }
 
+  function showRunningTimers() {
+    var i, runningTimer, runningTimers;
+
+    runningTimers = self.model.getRunningTimers();
+    for (i = runningTimers.length - 1; i >= 0; i -= 1) {
+      runningTimer = runningTimers[i];
+      showRunningTimer(runningTimer);
+    }
+  }
+
+  function showRunningTimer(runningTimer) {
+
+    var timerElement, runningTimersHeadline;
+
+    runningTimersHeadline = self.lib._('.runningTimers h3', true);
+    runningTimersMsg = self.lib._('.runningTimers .emptyMessage', true);
+    self.lib.addClass('hidden', runningTimersMsg);
+
+    timerElement = document.createElement('p');
+    timerElement.id = 'timer' + runningTimer.timerID;
+    timerElement.innerHTML =
+      'Timer "' + runningTimer.titleName + '": ' +
+      '<span>' + getFormatedTime(runningTimer.timerID) + '</span> ' +
+      '<a href="#" onclick="SimpleTimer.stop(' + runningTimer.timerID + ');">Stop</a>'
+    ;
+
+    self.lib.insertAfter(runningTimersHeadline, timerElement);
+    startIntervalForTimer(runningTimer.timerID);
+  }
+
+  function showStoppedTimers() {
+    var i, stoppedTimer, stoppedTimers;
+
+    stoppedTimers = self.model.getStoppedTimers();
+
+    for (i = stoppedTimers.length - 1; i >= 0; i -= 1) {
+      stoppedTimer = stoppedTimers[i];
+
+      showStoppedTimer(stoppedTimer);
+    }
+  }
+
+  function showStoppedTimer(stoppedTimer) {
+    var finishedTimersHeadline, finishedTimersMsg, timer, timerElement;
+
+    finishedTimersHeadline = self.lib._('.finishedTimers h3', true);
+    finishedTimersMsg = self.lib._('.finishedTimers .emptyMessage', true);
+
+    timerElement = document.createElement('p');
+    timerElement.id = 'timer' + stoppedTimer.timerID;
+    timerElement.innerHTML =
+      'Timer "' + stoppedTimer.titleName + '": ' +
+      '<span>' + getFormatedTime(stoppedTimer.timerID) + '</span> '
+    ;
+
+    self.lib.insertAfter(finishedTimersHeadline, timerElement);
+
+    self.lib.addClass('hidden', finishedTimersMsg);
+  }
+
+  function getTimers() {
+    var i, timers;
+
+    timers = self.model.getTimers();
+
+    return timers;
+  }
+
   self.init = function() {
-    if(!checkForStorageSupport())
+    if (!checkForStorageSupport())
       return;
 
     declareElements();
 
     bindEvents();
+
+    timers = getTimers();
+    showRunningTimers();
+    showStoppedTimers();
   };
 
   self.stop = function(timerID) {
@@ -198,19 +237,105 @@ SimpleTimer = (function() {
   };
 
   return self;
-}())
+}());
+
+SimpleTimer.model = (function() {
+  'use strict';
+
+  var self = SimpleTimer || {};
+
+  function isRunningTimer(timer) {
+    if (timer.endTime === 0) {
+      return true;
+    }
+
+    return false;
+  }
+
+  function isStoppedTimer(timer) {
+    if (isRunningTimer(timer)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  self.getTimers = function() {
+    var timers;
+
+    timers = self.dataHandler.get('timers');
+
+    if(typeof timers === 'undefined') {
+      timers = [];
+    }
+
+    return timers;
+  };
+
+  self.getRunningTimers = function() {
+    var timers, timer, runningTimers;
+
+    timers = self.model.getTimers();
+
+    runningTimers = [];
+    for (var i = timers.length - 1; i >= 0; i--) {
+      timer = timers[i];
+      timer.timerID = i;
+      if(isRunningTimer(timer)) {
+        runningTimers.push(timer);
+      }
+    };
+
+    return runningTimers;
+  };
+
+  self.getStoppedTimers = function() {
+    var timers, timer, stoppedTimers;
+
+    timers = self.model.getTimers();
+
+    stoppedTimers = [];
+    for (var i = timers.length - 1; i >= 0; i--) {
+      timer = timers[i];
+      timer.timerID = i;
+      if(isStoppedTimer(timer)) {
+        stoppedTimers.push(timer);
+      }
+    };
+
+    return stoppedTimers;
+  };
+
+  return self;
+}());
 
 SimpleTimer.dataHandler = (function() {
   'use strict';
 
   var self = SimpleTimer || {};
 
-  self.add = function(key, value) {
+  self.set = function(key, value) {
+    if (typeof value === 'object') {
+      value = JSON.stringify(value);
+    }
+
     localStorage.setItem(key, value);
   };
 
-  self.read = function(key) {
-    localStorage.getItem(key);
+  self.get = function(key) {
+    var value;
+
+    value = localStorage.getItem(key);
+
+    if (!value) {
+      return;
+    }
+
+    if (value[0] === '{' || value[0] === '[') {
+      value = JSON.parse(value);
+    }
+
+    return value;
   };
 
   self.remove = function(key) {
@@ -230,7 +355,7 @@ SimpleTimer.lib = (function() {
   var self = SimpleTimer || {};
 
   self._ = function(selector, getFirstFound) {
-    if(getFirstFound === true)
+    if (getFirstFound === true)
       return document.querySelector(selector);
     else
       return document.querySelectorAll(selector);
@@ -245,7 +370,7 @@ SimpleTimer.lib = (function() {
   };
 
   self.twoDigits = function(value) {
-    if(value < 10)
+    if (value < 10)
      return '0' + value;
 
     return value;
